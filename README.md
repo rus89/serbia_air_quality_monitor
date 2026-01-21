ac29429bacf4fb8bdca1f93eab4574c785a0182c
Milan Rusimov <rusmil89@gmail.com>
2026-01-21 15:42:13 +0100

    !X - Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# serbia_air_quality_monitor
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/serbia_air_quality_monitor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.serbia_air_quality_monitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
